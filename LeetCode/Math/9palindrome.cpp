6d7816623881e3887cc3c94e30aa348ea941c09a
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-08-08 02:01:19 +0600

    palindorm 9

---
+++ LeetCode/Math/9palindrome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+    class Solution {
+public:
+    bool isPalindrome(int x) {
+        int i=x,rem=0,result=0 ;
+
+        if(x < 0){
+            return false;
+        }
+        else{
+            while(x != 0){
+                result = result * 10 + x % 10;
+                rem = x%10;
+                
+                x = x/10;
+                cout<<result<<endl;
+            }
+            if(result == i){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+};
+
+
+
+    Solution sol;
+    int x = 121; // Example input
+    bool result = sol.isPalindrome(x);
+    cout << (result ? "true" : "false") << endl; // Output: true
+    return 0;
+}
+
